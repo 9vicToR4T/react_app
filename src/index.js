7a8127c5be0d3845eeb359b9e70bda12c11eabb7
victor <vt@mail.com>
2021-10-03 21:03:13 +0100

    react-app with create-react-app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const titleElement = <h1>Hello world!! My name is Victor.</h1>
+
+ReactDOM.render(titleElement, document.getElementById('root'))
